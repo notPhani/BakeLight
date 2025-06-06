f84ce9f3fa2dc57bf160cd2d4778b313d2dd143c
notPhani <phaniexplains14@gmail.com>
2025-06-06 23:25:32 +0530

    Refactor Sphere class to improve error handling and enhance intersection calculations

--- src/BakeLight/config.py
+++ src/BakeLight/config.py
@@ -2,7 +2,7 @@ import torch
 import os
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 EXPIREMENTAL = False
-
+EPSILON = 1e-6
 if EXPIREMENTAL:
     os.environ["PYTORCH_NO_CUDA_MEMORY_CACHING"] = 1
 else:
