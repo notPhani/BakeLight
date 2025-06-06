e26927a33d94ba2a5e86bc2837a9bb1c42d94163
notPhani <phaniexplains14@gmail.com>
2025-06-06 22:28:10 +0530

    Implement vector operations and normalization functions in math.py

--- src/BakeLight/core/math.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-def normailze(v: torch.Tensor) -> torch.Tensor:
-    norm = (v/ (torch.norm(v, dim=-1, keepdim=True)+ 1e-8))
-    return norm.to(device)
-
