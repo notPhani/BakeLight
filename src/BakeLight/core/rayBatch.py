8aaf2988285633fa824143f620d1b194da0c57c9
notPhani <phaniexplains14@gmail.com>
2025-06-06 22:44:40 +0530

    Add config.py to initialize device settings and print device information

---
+++ src/BakeLight/core/rayBatch.py
@@ -0,0 +1,14 @@
+import torch
+from BakeLight.config import DEVICE
+
+class RayBatch:
+    def __init__(self, origins, directions, device=DEVICE):
+        self.origins = torch.as_tensor(origins, device=device, dtype=torch.float32)
+        self.directions = torch.as_tensor(directions, device=device, dtype=torch.float32)
+        self.directions = self.directions / (torch.norm(self.directions, dim=-1, keepdim=True) + 1e-8)
+        if self.origins.shape[-1] != 3 or self.directions.shape[-1] != 3:
+            raise Exception("Ray origins/directions must be 3D vectors")
+    def at(self):
+        pass
+    def __repr__(self):
+        return f"Origins Shape : {self.origins.shape} \n Directions Shape: {self.directions.shape}\n"
